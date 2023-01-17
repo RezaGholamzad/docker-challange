16345b8ad1fe645e24604ade0c113176331dc920
r.gholamzad <gholamzadreza@gmail.com>
2023-01-17 18:55:13 -0500

    create project

---
+++ src/main/java/com/example/dockerchallange/DockerChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerChallangeApplication.class, args);
+    }
+
+}
