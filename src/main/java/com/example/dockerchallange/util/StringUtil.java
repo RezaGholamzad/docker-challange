3ef20684d837ca677a40fbc68054191defb2725c
r.gholamzad <gholamzadreza@gmail.com>
2023-01-17 19:00:43 -0500

    add stringUtil to split camel case

---
+++ src/main/java/com/example/dockerchallange/util/StringUtil.java
@@ -0,0 +1,14 @@
+package com.example.dockerchalllange1.util;
+
+public class StringUtil {
+    public static String splitByCamelCase (String input){
+        return input.replaceAll(
+                String.format("%s|%s|%s",
+                        "(?<=[A-Z])(?=[A-Z][a-z])",
+                        "(?<=[^A-Z])(?=[A-Z])",
+                        "(?<=[A-Za-z])(?=[^A-Za-z])"
+                ),
+                " "
+        );
+    }
+}
